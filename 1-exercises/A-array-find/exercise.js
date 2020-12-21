e223bf743e7efa1ec77680f05c10daf760de74be
Kos0606 <65927936+Kos0606@users.noreply.github.com>
2020-12-21 20:25:11 +0000

    JavaScript-Core-1-Coursework-week3

--- 1-exercises/A-array-find/exercise.js
+++ 1-exercises/A-array-find/exercise.js
@@ -17,7 +17,7 @@ var names = [
   "Ahmed",
 ];
 
-var longNameThatStartsWithA = findLongNameThatStartsWithA(names);
+var longNameThatStartsWithA = names.find(isLongName);
 
 console.log(longNameThatStartsWithA);
 
